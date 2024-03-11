6ac2978b8a8ed86ff5fdb912a07255e8f5ea35c5
kosssshhhh <kosukwhon@gmail.com>
2024-03-11 20:59:26 +0900

    [ :recycle: | HS2-12 | Refactor ] Memebers 페이지 컴포넌트화

---
+++ study-project/src/pages/members/_data/mock.js
@@ -0,0 +1,17 @@
+export const members = [
+	{ name: '강준우', department: '컴퓨터정보공학부', roles: ['#Full Stack', '#AI'], image: '/강준우.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Front End'], image: '/고석환.png' },
+	{ name: '서대원', department: '정보융합학부', roles: ['#Front End'], image: '/서대원.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Data Analyst', '#AI'], image: '/강준우.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Frontend', '#UX'], image: '/강준우.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Frontend'], image: '/강준우.png' },
+];
+
+export const tags = [
+	{ name: 'UX Researcher' },
+	{ name: 'AI' },
+	{ name: 'Data Analyst' },
+	{ name: 'Front End' },
+	{ name: 'Back End' },
+	{ name: 'Full Stack' },
+];
