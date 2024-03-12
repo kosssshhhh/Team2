71b0b35e446a7f7071c0ae4aea4f1ec4a6fa354f
kosssshhhh <kosukwhon@gmail.com>
2024-03-12 11:46:57 +0900

    [ :wrench: | HS2-12 | Fix ] 코딩 컨벤션에 맞게 컴포넌트 return

--- study-project/src/pages/members/page.jsx
+++ study-project/src/pages/members/page.jsx
@@ -8,13 +8,19 @@ export default function MembersPage() {
 			<div className="flex flex-col w-screen items-center bg-backgroundGrey p-20">
 				<div className="flex flex-col items-center mx-auto mb-10">
 					<h2 className="normal font-bold text-4xl text-textBlack">스터디 참여자</h2>
-					<p className="mt-5 text-base text-textLightBlack">HCCLab Study에 참여하고 있는 분들을 소개합니다.</p>
+					<p className="mt-7 text-base font-light text-textLightBlack">
+						HCCLab Study에 참여하고 있는 분들을 소개합니다.
+					</p>
 				</div>
 				<div className="flex flex-wrap item-center justify-center p-10">
-					{tags.map((tag, index) => Tag({ tag, index }))}
+					{tags.map((tag, index) => {
+						return Tag({ tag, index });
+					})}
 				</div>
 				<div className="flex flex-wrap justify-center px-20 ">
-					{members.map((member, index) => Member({ member, index }))}
+					{members.map((member, index) => {
+						return Member({ member, index });
+					})}
 				</div>
 			</div>
 		</main>
