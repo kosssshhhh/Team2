c6282be9189fe27b29865216effaf65dfc4b3609
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-12 17:25:53 +0900

    [ ✨ | HS2-11 | Feat ] 사용자 정보 입력 페이지 미리보기 구현

--- study-project/src/pages/login/student/info/page.jsx
+++ study-project/src/pages/login/student/info/page.jsx
@@ -1,13 +1,29 @@
+import { useRef, useState } from 'react';
 import Input from './Input';
 
 export default function UserInfoPage() {
+	const [imageFile, setImageFile] = useState(null);
+	const imageRef = useRef();
+
+	const handleInputImage = () => {
+		const file = imageRef.current.files[0];
+		const reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = () => {
+			setImageFile(reader.result);
+		};
+	};
+
 	return (
 		<div className="flex flex-col flex-grow w-11/12 h-full justify-start items-start gap-4 mx-auto">
 			<h2 className="text-2xl font-semibold mt-2 text-textBlack">입력하신 정보는 Members에 노출돼요</h2>
 			<label className="flex flex-col w-full gap-2">
 				<span className="text-sm font-semibold text-textBlack">프로필 사진 *</span>
-				<img className="w-full border-textBlack border h-56 rounded-lg" />
-				<input className="hidden" type="file" accept="image/*" />
+				<img
+					src={imageFile ? imageFile : null}
+					className="w-full border-textBlack border h-56 rounded-lg object-scale-down"
+				/>
+				<input className="hidden" type="file" accept="image/*" ref={imageRef} onChange={handleInputImage} />
 			</label>
 			<div className="w-full flex flex-col gap-5 my-4">
 				<Input title="성함" />
