c5aa94229c7ed45bf02ef715487ea8b53c937171
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-12 14:49:49 +0900

    [ ✨ | HS2-11 | Feat ] 로그인 레이아웃 넓이 조정

--- study-project/src/layout/LoginLayout/index.jsx
+++ study-project/src/layout/LoginLayout/index.jsx
@@ -5,7 +5,7 @@ export default function LoginLayout() {
 	return (
 		<div className="flex min-h-screen w-screen flex-col ">
 			<Header />
-			<div className="flex flex-col flex-grow mx-auto w-1/5">
+			<div className="flex flex-col flex-grow mx-auto w-1/4">
 				<Outlet />
 			</div>
 		</div>
--- study-project/src/pages/login/student/info/page.jsx
+++ study-project/src/pages/login/student/info/page.jsx
@@ -1,3 +1,7 @@
 export default function UserInfoPage() {
-	return <>학생정보</>;
+	return (
+		<div className="flex flex-col flex-grow w-full justify-start items-center gap-4">
+			<h2 className="text-2xl font-semibold mt-64">입력하신 정보는 Members에 노출돼요</h2>
+		</div>
+	);
 }
