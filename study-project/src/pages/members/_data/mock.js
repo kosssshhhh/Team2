16ba1590ea4130f60594b7beeab14fcc496ba837
kosssshhhh <kosukwhon@gmail.com>
2024-03-13 10:49:44 +0900

    [ :wrench: | HS2-12 | Fix ] image 이름 영어로 수정, member 흰색 blur 처리

--- study-project/src/pages/members/_data/mock.js
+++ study-project/src/pages/members/_data/mock.js
@@ -1,10 +1,10 @@
 export const members = [
-	{ name: '강준우', department: '컴퓨터정보공학부', roles: ['#Full Stack', '#AI'], image: '/강준우.png' },
-	{ name: '고석환', department: '정보융합학부', roles: ['#Front End'], image: '/고석환.png' },
-	{ name: '서대원', department: '정보융합학부', roles: ['#Front End'], image: '/서대원.png' },
-	{ name: '고석환', department: '정보융합학부', roles: ['#Data Analyst', '#AI'], image: '/강준우.png' },
-	{ name: '고석환', department: '정보융합학부', roles: ['#Frontend', '#UX'], image: '/강준우.png' },
-	{ name: '고석환', department: '정보융합학부', roles: ['#Frontend'], image: '/강준우.png' },
+	{ name: '강준우', department: '컴퓨터정보공학부', roles: ['#Full Stack', '#AI'], image: '/member1.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Front End'], image: '/member2.png' },
+	{ name: '서대원', department: '정보융합학부', roles: ['#Front End'], image: '/member3.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Data Analyst', '#AI'], image: '/member1.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Frontend', '#UX'], image: '/member2.png' },
+	{ name: '고석환', department: '정보융합학부', roles: ['#Frontend'], image: '/member3.png' },
 ];
 
 export const tags = [
