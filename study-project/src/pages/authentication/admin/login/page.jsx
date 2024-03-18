e5d1341020bd739a28026fc18b3e017f974f537c
suwonthugger <ddww1029wwdd@gmail.com>
2024-03-18 17:27:31 +0900

    [ ✨ | HS2-17 | Feat ] 관리자 로그인 화면 구현 및 Input 컴포넌트 생성

--- study-project/src/pages/authentication/admin/login/page.jsx
+++ study-project/src/pages/authentication/admin/login/page.jsx
@@ -1,3 +1,15 @@
+import Input from './input';
+
 export default function AdminLogin() {
-	return <>관리자 로그인</>;
+	return (
+		<div className="flex flex-col flex-grow w-11/12 h-full justify-start items-start gap-4 mx-auto">
+			<h2 className="text-5xl font-medium mt-32 text-textBlack mx-auto">Admin Login</h2>
+			<h3 className="text-kwRed text-sm">KW-HCC Lab 연구실에서 발급받은 관리자 아이디로 로그인해주세요</h3>
+			<div className="w-full flex flex-col gap-6 my-6">
+				<Input title="아이디" />
+				<Input title="비밀번호" />
+			</div>
+			<button className="bg-kwRed w-full px-2 py-3 rounded-lg text-white mt-4 text-center">로그인</button>
+		</div>
+	);
 }
