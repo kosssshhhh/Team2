321db8abbb89c7dd94b4d465683a0eb9e235a8a4
kosssshhhh <kosukwhon@gmail.com>
2024-03-19 13:43:35 +0900

    [ :wrench: | HS2-12 | Fix ] Members blur 진하게 Header 아래 줄 표시

--- study-project/src/layout/MainLayout/Header/index.jsx
+++ study-project/src/layout/MainLayout/Header/index.jsx
@@ -48,7 +48,7 @@ export default function Header() {
 			<header className="h-20 sticky top-0 w-full z-50 bg-white">
 				<div className="flex justify-between items-center h-20">
 					<Link to={routes.home.path} className="flex items-center">
-						<img src="/logo.png" alt="HCC Lab Logo" className="object-contain w-34 h-14 mx-4" />
+						<img src="logo.png" alt="HCC Lab Logo" className="object-contain w-34 h-14 mx-4" />
 					</Link>
 					<div className="flex justify-end items-center">
 						<ul className="mx-6">
@@ -58,7 +58,7 @@ export default function Header() {
 										key={index}
 										to={content.path}
 										className={`px-6  hover:text-black transition duration-300 ${
-											isActive(content.path) ? 'text-black' : 'text-textGrey'
+											isActive(content.path) ? 'text-black underline underline-offset-2' : 'text-textGrey'
 										}`}>
 										{content.title}
 									</Link>
